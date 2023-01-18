b2e33bb5fbaf9f67c917749861f207da80304fc7
karl wernsman <wernsmank@gmail.com>
2023-01-18 15:56:06 -0800

    country name and local names displaying

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,6 +1,12 @@
 import './Country.css';
 import React from 'react';
 
-export default function country() {
-  return <div>country</div>;
+export default function Country(country) {
+  return (
+    <div className="countryCard">
+      <h2>{country.name}</h2>
+      <h4>{country.local_name}</h4>
+      <img />
+    </div>
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,15 @@
 import './Main.css';
 import React from 'react';
+import { useCountries } from '../../hooks/countries.js';
+import Country from '../Country/Country.js';
 
 export default function Main() {
-  return <div>Main</div>;
+  const { countries } = useCountries();
+  return (
+    <main>
+      {countries.map((country) => (
+        <Country key={country.id} {...country} />
+      ))}
+    </main>
+  );
 }
