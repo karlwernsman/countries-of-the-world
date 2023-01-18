014c3c4aaf39136d211443206c0a3e3a929e20ec
karl wernsman <wernsmank@gmail.com>
2023-01-18 15:06:55 -0800

    setup countries Hook using useEffect and useState in countries.js

---
+++ src/hooks/countries.js
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { getCountries } from '../services/countries.js';
+
+export function useCountries() {
+  const [countries, setCountries] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await getCountries();
+        setCountries(res);
+      } catch (e) {
+        setError('Uh oh! Not gonna work.');
+      }
+    };
+    fetchData();
+  }, []);
+  return { countries, error };
+}
