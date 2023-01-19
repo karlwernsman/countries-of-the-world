128621853faba743d945e879ee3c3d36307c8204
karl wernsman <wernsmank@gmail.com>
2023-01-18 16:19:05 -0800

    flag images displaying

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -6,7 +6,7 @@ export default function Country(country) {
     <div className="countryCard">
       <h2>{country.name}</h2>
       <h4>{country.local_name}</h4>
-      <img />
+      <img src={`https://flagcdn.com/w320/${country.iso2.toLowerCase()}.png`} />
     </div>
   );
 }
